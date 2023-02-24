5345037be325db49b5a95a0f79c6f04e81a110aa
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2023-02-24 14:20:58 +0700

    Update clearScreen.go

--- clearScreen.go
+++ clearScreen.go
@@ -10,21 +10,21 @@ import (
 
 // Initialization clearScreen() function
 func clearScreen(){
-  // Get OS name and save it into '_os' variable
+  	// Get OS name and save it into '_os' variable
 	_os := runtime.GOOS
 
-  // logic 1: windows
+  	// logic 1: windows
 	if _os == "windows" {
 		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
-  } // logic 2: linux
-  else if _os == "linux" {
+  	} // logic 2: linux
+  	else if _os == "linux" {
 		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
-  } // logic 3: darwin, etc..
-  else {
+ 	 } // logic 3: darwin, etc..
+  	else {
 		fmt.Print()
 	}
 }
